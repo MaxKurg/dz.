b814aaddf0d6620b608be84203e4142e531c407f
84187 <8418719@gmail.com>
2022-04-05 15:38:21 +0300

    jj

---
+++ main.py
@@ -0,0 +1,14 @@
+def z_p(a, b, c):
+    print(a * b + c)
+num1 = int(input("введите количество часов: "))
+num2 = 100
+num3 = 1000
+z_p(num1,num2,num3)
+
+#
+for q in range(20,240,20):
+    print(q)
+number = [300,2,12,44,1,1,4,10,7,1,78,123,55]
+number.sort()
+
+
